3b7b50bf58d1294df472df54865d0f5750e6b729
Jusung Kye <kyejusung@gmail.com>
2015-07-25 01:00:31 +0900

    some test change

--- UnitTestsCardExampleTests/CardTestCase.m
+++ UnitTestsCardExampleTests/CardTestCase.m
@@ -19,11 +19,13 @@
 - (void)setUp {
     [super setUp];
     NSLog(@"%s",__PRETTY_FUNCTION__);
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
     NSLog(@"%s",__PRETTY_FUNCTION__);
     [super tearDown];
 }
